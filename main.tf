183054bfd79f4d19024b70f7cd57ec6119450b87
fuckerlo <88121812+fuckerlo@users.noreply.github.com>
2022-06-15 15:09:22 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "ansible_host" "salt-proxy" {
   count = 1
 
   // 配置机器的 hostname，一般配置为计算资源的 public_ip (或 private_ip)
-  inventory_hostname  = alicloud_instance.ecsone[count.index].public_ip
+  inventory_hostname  = "39.103.71.91"
 
   // 配置机器所属分组
   groups = ["salt-proxy"]
@@ -45,7 +45,7 @@ resource "ansible_host" "salt-proxy" {
   // 传给 ansible 的 vars，可在 playbook 文件中引用
   vars = {
     wait_connection_timeout   = 60
-    proxy_private_ip          = alicloud_instance.ecsone[count.index].private_ip
+    proxy_private_ip          = "172.27.159.213"
     proxy_docker_tag          = var.proxy_docker_tag
   }
 }
