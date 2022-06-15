90c6ceb5c281b2bdf64a768c1e37f619df715d55
fuckerlo <88121812+fuckerlo@users.noreply.github.com>
2022-06-15 17:01:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,71 +8,15 @@ terraform {
 	 	source ="aliyun/alicloud"
 	 	version ="1.99.0"
 	 }
-	 docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-   }
-	}
+}
 }
 provider "ansible" {
-	
 }
 provider "alicloud" {
 	access_key = "LTAI5tCWCZozc13cm4YsXZuU"
         secret_key = "v7CBTYeowvR9ztFZUvktWUp1yK9ZK1"
         region = "cn-beijing"
 }
-resource "alicloud_vpc" "vpc" {
-  name       = "tf_test_foo"
-  cidr_block = "172.16.0.0/12"
-}
-
-resource "alicloud_vswitch" "vsw" {
-  vpc_id            = alicloud_vpc.vpc.id
-  cidr_block        = "172.16.0.0/21"
-  availability_zone = "cn-beijing-b"
-}
-
-resource "alicloud_security_group" "default" {
-  name = "default"
-  vpc_id = alicloud_vpc.vpc.id
-}
-
-resource "alicloud_instance" "instance" {
-  # cn-beijing
-  availability_zone = "cn-beijing-b"
-  security_groups = alicloud_security_group.default.*.id
-  # series III
-  instance_type        = "ecs.n2.small"
-  system_disk_category = "cloud_efficiency"
-  image_id             = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
-  instance_name        = "test_foo"
-  vswitch_id = alicloud_vswitch.vsw.id
-  internet_max_bandwidth_out = 10
-}
-
-resource "alicloud_security_group_rule" "allow_all_tcp" {
-  type              = "ingress"
-  ip_protocol       = "tcp"
-  nic_type          = "intranet"
-  policy            = "accept"
-  port_range        = "1/65535"
-  priority          = 1
-  security_group_id = alicloud_security_group.default.id
-  cidr_ip           = "0.0.0.0/0"
-}
-provider "docker" {}
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
- internal = 80
-    external = 8000
- }
 }
 resource "ansible_host" "salt-proxy" {
   count = 1
