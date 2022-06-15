ba64dd905e1fce990f1793a1cf6d87d9c6e6ba09
fuckerlo <88121812+fuckerlo@users.noreply.github.com>
2022-06-15 16:06:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,9 @@ provider "ansible" {
 	
 }
 provider "alicloud" {
-	
+	access_key = "LTAI5tCWCZozc13cm4YsXZuU"
+        secret_key = "v7CBTYeowvR9ztFZUvktWUp1yK9ZK1"
+        region = "cn-beijing"
 }
 resource "alicloud_vpc" "vpc" {
   name       = "tf_test_foo"
